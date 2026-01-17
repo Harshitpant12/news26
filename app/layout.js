967fa8427394f2582afccbce31f35d5ae9b1bd23
Harshitpant12 <harshpant778@gmail.com>
2026-01-17 12:14:40 +0530

    Update

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
+            <head>
+                <meta name="google-site-verification" content="260f36605ddf5f02" />
+            </head>
             <body>
                 <HeaderClient />
                 <main style={main}>{children}</main>

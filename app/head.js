fb14eddfcbb37b8c209d8d39c5bf3407966a660d
Harshitpant12 <harshpant778@gmail.com>
2026-01-17 12:10:48 +0530

    Add head.js

---
+++ app/head.js
@@ -0,0 +1,10 @@
+export default function Head() {
+    return (
+        <>
+            <meta
+                name="google-site-verification"
+                content="260f36605ddf5f02"
+            />
+        </>
+    );
+}

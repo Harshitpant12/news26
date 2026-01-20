a76d2b8bc50fdb03cc590b67e7d10273459fc4b1
Harshitpant12 <harshpant778@gmail.com>
2026-01-20 11:19:25 +0530

    Add

--- app/google260f36605ddf5f02.html/route.js
+++
@@ -1,9 +0,0 @@
-export async function GET() {
-    return new Response(
-        "google-site-verification: google260f36605ddf5f02.html",
-        {
-            status: 200,
-            headers: { "Content-Type": "text/plain" },
-        }
-    );
-}
